64b27ea3ce5be8461a97e1c256802afc895575fe
GGD <55097456+theotheperson10@users.noreply.github.com>
2022-05-11 09:22:35 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var x=0;
+var y=0;
+var w=20;
+var screenW=1600;
+var screenH=1600;
+function setup() {
+  createCanvas(screenW, screenH);
+}
+
+function draw() {
+  background(220);
+  while(x*y<=screenW*screenH){
+    fill(x,(x+y)/2,y);
+    noStroke();
+rect(x,y,w,w);
+  x+=w;
+  if(x>=screenW){
+  x=0;
+  y+=w;}
+  }
+  x=0;
+  y=0;
+  if(key=="a"){w+=0.5;}
+  if(key=="d"){if(w-1>0){w-=0.5}}
+}
