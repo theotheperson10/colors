45f365847c6bb62dc42a4bc264dfedb99f45226b
GGD <55097456+theotheperson10@users.noreply.github.com>
2022-05-12 09:39:43 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
 var x=0;
 var y=0;
 var w=20;
-var screenW=1000;
-var screenH=1000;
+var screenW=800;
+var screenH=800;
 function setup() {
   createCanvas(screenW, screenH);
   
@@ -12,10 +12,10 @@ function draw() {
   background(220);
   while(x*y<=screenW*screenH){
     stroke(x,(x+y)/2,y);
-    strokeWeight(w/2)
+    strokeWeight(w*2)
     line(x,y,mouseX,mouseY);
   x+=w;
-  if(x>=screenW){
+  if(x>=screenW+w){
   x=0;
   y+=w;}
   }
